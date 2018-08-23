dcf7f4db77e82052ee3fa1e02f349eb3bfef5ec9
arun0818 <42594485+arun0818@users.noreply.github.com>
2018-08-24 00:50:48 +1000

    Update index.js

--- index.js
+++ index.js
@@ -22,34 +22,30 @@ airlineDetails.forEach(function (airlineDetail) {
   if(req.body.queryResult.parameters.departCity == airlineDetail.departingCity && req.body.queryResult.parameters.arrivalCity == airlineDetail.arrivalCity)
   {
   counter = counter+1;  
-  var cp = calculateCarbonFootPrint(airlineDetail.stops,airlineDetail.luggage,airlineDetail.runningHours,airlineDetail.fuelEfficiency);
+  //var cp = calculateCarbonFootPrint(airlineDetail.stops,airlineDetail.luggage,airlineDetail.runningHours,airlineDetail.fuelEfficiency);
+  var cp = "unclassified";
+    
+    if (airlineDetail.stops < 2  && airlineDetail.luggage < 15 && airlineDetail.runningHours < 12 && airlineDetail.fuelEfficiency > 0.85 )
+   {
+     cp = "low";
+   }
+   
+   if (airlineDetail.stops < 3  && airlineDetail.luggage < 15 && airlineDetail.runningHours < 15 && airlineDetail.fuelEfficiency > 0.75 && airlineDetail.fuelEfficiency < 0.85 )
+   {
+     cp = "average";
+   } 
+       
+   if (airlineDetail.stops > 3 || airlineDetail.luggage > 15 || airlineDetail.runningHours > 15 || airlineDetail.fuelEfficiency < 0.75 )
+   {
+     cp = "very high";
+   }
+    
   chatResponse = chatResponse + "\n\r\t" +"Option :" +counter+" "+airlineDetail.name+ " which takes "+airlineDetail.runningHours+" hours and has "+airlineDetail.stops+" stops. The carbon footprint is \n\r\t"+cp+"." ;
   }
   
 });
   
-  function calculateCarbonFootPrint(stops,luggage,runningHours,fuelEfficiency)
-  {
-    var cp = "high" ;
-   /*if (stops < 2  luggage < 15 && runningHours < 12 && fuelEfficiency > 0.85 )
-   {
-     cp = "low";
-   }
-   
-   if (stops < 3  luggage < 15 && runningHours < 15 && fuelEfficiency > 0.75 && fuelEfficiency < 0.85 )
-   {
-     cp = "average";
-   } 
-    
-    
-   if (stops > 3 || luggage > 15 || runningHours > 15 || fuelEfficiency < 0.75 )
-   {
-     cp = "very high";
-   } */
-   return cp;
-  }
-  
- 
+
    
   return res.json({
     fulfillmentText: chatResponse
