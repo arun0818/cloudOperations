6098102b0c45105b0631b04d037bee2a23a45b08
arun0818 <42594485+arun0818@users.noreply.github.com>
2018-08-23 23:19:35 +1000

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,15 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-var chatResponse = "default value";
+var chatResponse = "";
+var counter = 0;  
 var airlineDetails = require('./airline.json');
 airlineDetails.forEach(function (airlineDetail) {
   //chatResponse = airlineDetail.departingCity;
   if(req.body.queryResult.parameters.departCity == airlineDetail.departingCity && req.body.queryResult.parameters.arrivalCity == airlineDetail.arrivalCity)
   {
-  chatResponse = chatResponse + 'Option : ' +airlineDetail.name+ ' which takes '+airlineDetail.runningHours+' hours and has '+airlineDetail.stops+' stops.';
+  counter = counter+1;  
+  chatResponse = chatResponse + 'Option :' +counter+' '+airlineDetail.name+ ' which takes '+airlineDetail.runningHours+' hours and has '+airlineDetail.stops+' stops.';
   }
   
 });
